7aa1196135bbae5984c175ec889055131d07b83c
Gordon M. Celesta <gordo@sdf.lonestar.org>
2019-02-09 12:10:26 -0800

    Reworked with scheduler

--- iss.py
+++ iss.py
@@ -5,17 +5,28 @@ import time
 
 lat = "48.4225"
 lon = "-123.3585"
+sked = sched.scheduler(time.time, time.sleep)
 
-def alertme(jsoncontent):
-    return time.ctime(jsoncontent['response'][0]['risetime'] - 300)
+def foo():
+    print("Hello ISS")
+
+def getnextalert(jsoncontent):
+    return int(jsoncontent['response'][0]['risetime'] - 800 - time.time())
+
+def addsked(nextalert):
+    print(nextalert)
+    sked.enter(nextalert, 1, foo)
+    sked.run()
+    # Call function to perform alert operation
 
 def main():
+    # Need to loop
+
     #http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON
     content = requests.get("http://api.open-notify.org/iss-pass.json?lat=" + lat + "&lon=" + lon)
-
     jsoncontent = content.json()
-
-    print(alertme(jsoncontent))
+    print(jsoncontent)
+    addsked(getnextalert(jsoncontent))
 
 if __name__ == "__main__":
     main()
