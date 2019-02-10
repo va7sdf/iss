94b5bade75a25aa1828262f310025f2c298bae42
Gordon M. Celesta <gordo@sdf.lonestar.org>
2019-02-10 09:33:51 -0800

    Add altitude and only get next pass

--- iss.py
+++ iss.py
@@ -6,32 +6,38 @@ import time
 # location variables
 lat = "48.4225"
 lon = "-123.3585"
-
-# seconds for early warning
-earlywarn = 300
+alt = "8"
 
 # initialize scheduler
 sked = sched.scheduler(time.time, time.sleep)
 
-def doalert():
+def doalert(duration):
+
+    # start alert
     print("Hello ISS")
+
+    time.sleep(duration)
+
+    print("Goodbye ISS")
 
 def getnextalert():
     "Return the number of seconds until the next flyover"
-    content = requests.get("http://api.open-notify.org/iss-pass.json?lat=" + lat + "&lon=" + lon)
+    content = requests.get("http://api.open-notify.org/iss-pass.json?lat=" + lat + "&lon=" + lon + "&alt=" + alt + "&n=1")
     jsoncontent = content.json()
-    return int(jsoncontent['response'][0]['risetime'] - earlywarn - time.time())
+    return (int(jsoncontent['response'][0]['risetime'] - time.time()),
+            int(jsoncontent['response'][0]['duration']))
 
-def addsked(nextalert):
+def addsked(nextalert, duration):
     "Schedule next alert"
-    print(nextalert)
-    sked.enter(nextalert, 1, doalert)
+    sked.enter(nextalert, 1, doalert, {duration})
     sked.run()
 
 def main():
     # Need to loop
-    seconds = getnextalert()
-    addsked(seconds)
+    alerttime, alertduration = getnextalert()
+    print(alerttime)
+    print(alertduration)
+    addsked(alerttime, alertduration)
 
 if __name__ == "__main__":
     main()
