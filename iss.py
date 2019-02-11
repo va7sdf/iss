ed32dfbe93b1eb754e1c028fe87c08d9ec42e3db
Gordon M. Celesta <gordo@sdf.lonestar.org>
2019-02-11 05:35:42 -0800

    Schedule five events

--- iss.py
+++ iss.py
@@ -12,6 +12,7 @@ alt = "8"
 sked = sched.scheduler(time.time, time.sleep)
 
 def doalert(duration):
+    "Perform alert"
 
     # start alert
     print("Hello ISS")
@@ -21,40 +22,35 @@ def doalert(duration):
     # end alert
     print("Goodbye ISS")
 
-def getnextalert():
-    "Return the number of seconds until and duration of the next flyover"
+def alerts():
+    "Parse JSON file and schedule next five alerts"
+
     url = 'http://api.open-notify.org/iss-pass.json'
-    querystring = {'lat':lat, 'lon':lon, 'alt':alt, 'n':'1'}
+    querystring = {'lat':lat, 'lon':lon, 'alt':alt}
     content = requests.get(url, querystring)
     # Print response headers
-    #print(content.headers)
+    print(content.headers)
     jsoncontent = content.json()
     # Print content of json download
-    #print(jsoncontent)
+    print(jsoncontent)
 
-    # Print server and local times
-    print("Server time: " + time.ctime(jsoncontent['request']['datetime']))
-    print("Local time:  " + time.ctime())
+    for x in range(0, 5):
+        risetime = jsoncontent['response'][x]['risetime']
+        duration = jsoncontent['response'][x]['duration']
+        # Print next rise and duration in human readable form
+        print("risetime: " + time.ctime(risetime))
+        print("duration: " + str(duration) + " seconds")
 
-    risetime = jsoncontent['response'][0]['risetime']
-    duration = jsoncontent['response'][0]['duration']
-    # Print next rise and duration in human readable form
-    print("risetime: " + time.ctime(risetime))
-    print("duration: " + str(duration) + " seconds")
+        sked.enter(int(risetime - time.time()), 1, doalert, {duration})
 
-    return (int(risetime - time.time()), duration)
-
-def addsked(nextalert, duration):
-    "Schedule next alert"
-    sked.enter(nextalert, 1, doalert, {duration})
     # Print queue of scheduled tasks
-    # print(sked.queue)
+    print(sked.queue)
+
     sked.run()
 
 def main():
     while True:
-        alerttime, alertduration = getnextalert()
-        addsked(alerttime, alertduration)
+        alerts()
 
 if __name__ == "__main__":
     main()
